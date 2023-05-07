f49e21cc521d7853fec10d7c5dbdb9405a5b3b19
AlexandraKuraeva <alexa.kurav@yandex.ru>
2023-05-07 16:50:46 +0400

    Верстка модального окна

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import final from '/img/catalog/final.svg';
+import close from '/img/catalog/close.svg';
 type ModalProps = {
   active: boolean;
   setActive: React.Dispatch<React.SetStateAction<boolean>>;
@@ -12,9 +14,13 @@ const Modal: React.FC<ModalProps> = ({ active, setActive }) => {
           onClick={(e) => e.stopPropagation()}
         >
           <div className="modal__inner">
-            <h3 className="modal__title">Спасибо за заказ</h3>
+            <span onClick={() => setActive(false)}>
+              <img src={close} alt="закрыть" />
+            </span>
 
-            
+            <img src={final} alt="завершить заказ" />
+            <h3 className="modal__title">Спасибо за заказ</h3>
+            <p>Наш менеджер свяжется с вами в ближайшее время</p>
           </div>
         </div>
       </div>
